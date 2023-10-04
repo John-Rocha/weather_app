bff53fce45930fd5043588af2a4f8a8d3fa78c3a
John Rocha <jrocha@tractian.com>
2023-10-04 12:24:12 -0300

    feat: removed unecessary import

--- lib/data/data_sources/weather_remote_data_source.dart
+++ lib/data/data_sources/weather_remote_data_source.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:weather_app/core/contants/constants.dart';
 import 'package:weather_app/core/error/exceptions.dart';
-import 'package:weather_app/core/error/failure.dart';
 import 'package:weather_app/data/models/weather_model.dart';
 
 abstract class WeatherRemoteDataSource {
