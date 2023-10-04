3ea2a9581865eaf2cc796859236a4c1215904fd4
John Rocha <jrocha@tractian.com>
2023-10-04 12:30:18 -0300

    feat: test for fetch error

--- test/data/data_sources/remote_data_source_test.dart
+++ test/data/data_sources/remote_data_source_test.dart
@@ -3,6 +3,7 @@ import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 import 'package:weather_app/core/contants/constants.dart';
+import 'package:weather_app/core/error/exceptions.dart';
 import 'package:weather_app/data/data_sources/weather_remote_data_source.dart';
 import 'package:weather_app/data/models/weather_model.dart';
 import 'package:weather_app/domain/repositories/weather_repository.dart';
@@ -41,5 +42,24 @@ void main() {
       // assert
       expect(result, isA<WeatherModel>());
     });
+
+    test(
+      'should throw a server exception when the response code is 404 or other',
+      () async {
+        // arrange
+        when(mockHttpClient.get(
+          Uri.parse(Constants.currentWeatherByName(cityName)),
+        )).thenAnswer(
+          (_) async => http.Response(
+            'Not Found',
+            404,
+          ),
+        );
+        // act
+        final result = weatherRemoteDataSourceImpl.getCurrentWeather(cityName);
+        // assert
+        expect(result, throwsA(isA<ServerException>()));
+      },
+    );
   });
 }
