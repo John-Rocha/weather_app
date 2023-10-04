61bbb39192e3b91d67bebdc94a77850c129606a3
John Rocha <jrocha@tractian.com>
2023-10-04 14:45:14 -0300

    feat: defines bloc

--- lib/presentation/bloc/weather_bloc.dart
+++ lib/presentation/bloc/weather_bloc.dart
@@ -1,13 +1,38 @@
-import 'package:bloc/bloc.dart';
+import 'dart:async';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rxdart/transformers.dart';
+import 'package:weather_app/domain/entities/weather_entity.dart';
+import 'package:weather_app/domain/usecases/get_current_weather.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc() : super(WeatherInitial()) {
-    on<WeatherEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final GetCurrentWeatherUseCase _getCurrentWeatherUseCase;
+
+  WeatherBloc({required GetCurrentWeatherUseCase getCurrentWeatherUseCase})
+      : _getCurrentWeatherUseCase = getCurrentWeatherUseCase,
+        super(WeatherEmpty()) {
+    on<OnCityChanged>(
+      onCityChanged,
+      transformer: debounce(const Duration(milliseconds: 500)),
+    );
   }
+
+  Future<FutureOr<void>> onCityChanged(
+    OnCityChanged event,
+    Emitter<WeatherState> emit,
+  ) async {
+    emit(WeatherLoading());
+    final result = await _getCurrentWeatherUseCase.execute(event.cityName);
+    result.fold(
+      (failure) => emit(WeatherLoadError(failure.message)),
+      (weatherEntity) => emit(WeatherLoaded(weatherEntity)),
+    );
+  }
+}
+
+EventTransformer<T> debounce<T>(Duration duration) {
+  return (events, mapper) => events.debounceTime(duration).flatMap(mapper);
 }
--- lib/presentation/bloc/weather_state.dart
+++ lib/presentation/bloc/weather_state.dart
@@ -1,10 +1,30 @@
 part of 'weather_bloc.dart';
 
-sealed class WeatherState extends Equatable {
+abstract class WeatherState extends Equatable {
   const WeatherState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-final class WeatherInitial extends WeatherState {}
+final class WeatherEmpty extends WeatherState {}
+
+final class WeatherLoading extends WeatherState {}
+
+final class WeatherLoaded extends WeatherState {
+  final WeatherEntity weatherEntity;
+
+  const WeatherLoaded(this.weatherEntity);
+
+  @override
+  List<Object> get props => [weatherEntity];
+}
+
+final class WeatherLoadError extends WeatherState {
+  final String message;
+
+  const WeatherLoadError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
