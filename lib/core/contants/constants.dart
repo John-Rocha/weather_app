86ea8c93b4f97169eb7ffcbb114c3f3b0dded23f
John Rocha <jrocha@tractian.com>
2023-10-04 16:09:29 -0300

    feat: defines unit

--- lib/core/contants/constants.dart
+++ lib/core/contants/constants.dart
@@ -1,9 +1,10 @@
 class Constants {
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
   static const String apiKey = 'ac092e7a6b4431c1c3f27cb493eb616d';
+  static const String units = 'metric';
 
   static String currentWeatherByName(String cityName) {
-    return '$baseUrl/weather?q=$cityName&appid=$apiKey';
+    return '$baseUrl/weather?q=$cityName&units=$units&appid=$apiKey';
   }
 
   static String weatherIcon(String iconCode) {
