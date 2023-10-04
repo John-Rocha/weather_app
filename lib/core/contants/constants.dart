a247cc17c7ec740cb142690dd7fb1d0191ffb7ec
John Rocha <jrocha@tractian.com>
2023-10-04 12:10:41 -0300

    feat: created constants

---
+++ lib/core/contants/constants.dart
@@ -0,0 +1,12 @@
+class Constants {
+  static const String baseUrl = 'https://api.openweathermap.org/data/2.5/';
+  static const String apiKey = 'ac092e7a6b4431c1c3f27cb493eb616d';
+
+  static String currentWeatherByName(String cityName) {
+    return '$baseUrl/weather?q=$cityName&appid=$apiKey';
+  }
+
+  static String weatherIcon(String iconCode) {
+    return 'http://openweathermap.org/img/wn/$iconCode@2x.png';
+  }
+}
