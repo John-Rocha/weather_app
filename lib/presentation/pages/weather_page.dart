9d743d0e8fad57dd8200007727d8857f8062e800
John Rocha <jrocha@tractian.com>
2023-10-05 12:12:19 -0300

    chore: removed unecessary import

--- lib/presentation/pages/weather_page.dart
+++ lib/presentation/pages/weather_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/core/contants/constants.dart';
