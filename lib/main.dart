a9e5e5982f2c8b8cd3a5afe8ad3b90688a3e4144
John Rocha <jrocha@tractian.com>
2023-10-05 15:59:57 -0300

    feat: configure firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_app/app_widget.dart';
+import 'package:weather_app/firebase_options.dart';
 import 'package:weather_app/injection_container.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   setupLocator();
   runApp(const AppWidget());
 }
